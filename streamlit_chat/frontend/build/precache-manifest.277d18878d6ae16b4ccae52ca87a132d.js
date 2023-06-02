1bf90f99a354efb4d390d137b11b11beeb10806e
Pascal Heßler <pascal.hessler@wirtschaft.uni-giessen.de>
2023-06-02 12:10:43 +0200

    build

---
+++ streamlit_chat/frontend/build/precache-manifest.277d18878d6ae16b4ccae52ca87a132d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbba571acb9041e2dd9f28a777f94ba4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "eff46949148898e68404",
+    "url": "./static/js/2.ce1c52c8.chunk.js"
+  },
+  {
+    "revision": "3fc7fb5bfeeec1534560a2c962e360a7",
+    "url": "./static/js/2.ce1c52c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdee2ea485658838f361",
+    "url": "./static/js/main.338a4075.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
